195a693402dc0a667f432f7232632ee9596485ae
Jamezfn <jamesmakachia19@gmail.com>
2024-09-27 00:39:06 -0700

    printlist

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,7 @@
 
 /**
  * print_dlistint - prints all the elements of a dlistint_t list.
- * @h: pointer the head of the 
+ * @h: pointer the head of the doubly linked list
  * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
